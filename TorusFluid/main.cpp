5977ac65401e35d4b1bfb7ff6bfe3a0e238e64ff
sparkasaurusRex <ellis.sparky.hoag@gmail.com>
2015-09-04 21:36:25 -0500

    Initial Commit

---
+++ TorusFluid/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TorusFluid
+//
+//  Created by Ellis Sparky Hoag on 9/4/15.
+//  Copyright © 2015 Ellis Sparky Hoag. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
